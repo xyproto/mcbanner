94b56097d9b13895448042066e4338c5dffb33f1
Alexander Rødseth <rodseth@gmail.com>
2014-11-10 14:16:37 +0100

    Generating almost all patterns now

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ const (
 	halfH  = bannerH / 2
 	thirdW = bannerW / 3
 	thirdH = bannerH / 3
+	fifthW = bannerW / 5
+	fifthH = bannerH / 5
+	sixthW = bannerW / 6
+	sixthH = bannerH / 6
+	tenthW = bannerW / 10
+	tenthH = bannerH / 10
 
 	maxX = fullW - 1
 	maxY = fullH - 1
@@ -140,45 +146,142 @@ func (b *Banner) Draw(svg *onthefly.Tag) {
 		case patternUpperThird: // Chief Fess Banner
 			svg.Box(0, 0, fullW, thirdH, color)
 		case patternLeftThird: // Pale Dexter Banner
+			svg.Box(0, 0, thirdW, fullH, color)
 		case patternRightThird: // Pale Sinister Banner
+			svg.Box(fullW-thirdW, 0, thirdW, fullH, color)
 		case patternCenterThird: // Pale Banner
 			svg.Box(halfW-(thirdW/2), 0, thirdW, fullH, color)
 		case patternHorizontalLine: // Fess Banner
-			svg.Box(0, halfH-1, fullW, 2, color)
+			svg.Box(0, halfH-1, fullW, thirdW/2, color)
 		case patternDiagonal1: // Bend Banner
+			svg.Line(0, 0, fullW, fullH, thirdW, color)
 		case patternDiagonal2: // Bend Sinister Banner
+			svg.Line(fullW, 0, 0, fullH, thirdW, color)
 		case patterStripes: // Paly Banner
+			for i := 0.15; i < 1.0; i += 0.25 {
+				x := int(float64(fullW) * i)
+				svg.Line(x, 0, x, fullH, sixthW, color)
+			}
 		case patternDiaCross: // Saltire Banner
+			svg.Line(0, 0, fullW, fullH, thirdW, color)
+			svg.Line(fullW, 0, 0, fullH, thirdW, color)
 		case patternCross: // Cross Banner
+			svg.Line(0, halfH, fullW, halfH, sixthW, color)
+			svg.Line(halfW, 0, halfW, fullH, sixthW, color)
 		case patternUpperLeftTriangle: // Per Bend Sinister Banner
+			svg.Triangle(0, 0, fullW, 0, 0, fullH, color)
 		case patternUpperRightTriangle: // Per Bend Banner
+			svg.Triangle(0, 0, fullW, 0, fullW, fullH, color)
 		case patternLowerLeftTriangle: // Per Bend Inverted Banner
+			svg.Triangle(0, fullH, 0, 0, fullW, fullH, color)
 		case patternLowerRightTriangle: // Per Bend Sinister Inverted Banner
+			svg.Triangle(0, fullH, fullW, 0, fullW, fullH, color)
 		case patternLeftHalf: // Per Pale Banner
+			svg.Box(0, 0, halfW, fullH, color)
 		case patternRightHalf: // Per Pale Inverted Banner
+			svg.Box(halfW, 0, halfW, fullH, color)
 		case patternUpperHalf: // Per Fess Banner
 			svg.Box(0, 0, fullW, halfH, color)
 		case patternLowerHalf: // Per Fess Inverted Banner
 			svg.Box(0, halfH, fullW, halfH, color)
 		case patternLowerLeftSquare: // Base Dexter Canton Banner
+			svg.Box(0, fullH-thirdH, halfW, thirdH, color)
 		case patternLowerRightSquare: // Base Sinister Canton Banner
+			svg.Box(halfW, fullH-thirdH, halfW, thirdH, color)
 		case patternUpperLeftSquare: // Chief Dexter Canton Banner
+			svg.Box(0, 0, halfW, thirdH, color)
 		case patternUpperRightSqaure: // Chief Sinister Canton Banner
+			svg.Box(halfW, 0, halfW, thirdH, color)
 		case patternLowerTriangle: // Chevron Banner
 			svg.Triangle(0, fullH, fullW, fullH, halfW, halfH, color)
 		case patternUpperTriangle: // Inverted Chevron Banner
 			svg.Triangle(0, 0, fullW, 0, halfW, halfH, color)
 		case patternLowerWaves: // Base Indented Banner
+			svg.Ellipse(sixthW, fullH, fifthW, thirdW, color)
+			svg.Ellipse(halfW, fullH, fifthW, thirdW, color)
+			svg.Ellipse(fullW-sixthW, fullH, fifthW, thirdW, color)
 		case patternUpperWaves: // Chief Indented Banner
+			svg.Ellipse(sixthW, 0, fifthW, thirdW, color)
+			svg.Ellipse(halfW, 0, fifthW, thirdW, color)
+			svg.Ellipse(fullW-sixthW, 0, fifthW, thirdW, color)
 		case patternCircle: // Roundel Banner
 			svg.Circle(halfW, halfH, thirdW, color)
 		case patternDiamond: // Lozenge Banner
+			svg.Poly4(halfW, fifthH,
+				fullW-fifthW, halfH,
+				halfW, fullH-fifthH,
+				fifthW, halfH,
+				color)
 		case patternBorder: // Bordure Banner
+			svg.Line(0, 0, fullW, 0, sixthW, color)
+			svg.Line(fullW, 0, fullW, fullH, sixthW, color)
+			svg.Line(0, fullH, fullW, fullH, sixthW, color)
+			svg.Line(0, 0, 0, fullH, sixthW, color)
 		case patternWaveBorder: // Black/Dyed Borduer Indented Banner
+			br := halfH             // radius of circles in corners
+			ofs1 := fifthH          // offset away from the center, corner circles
+			brx := sixthH           // radius x of circles along the sides
+			bry := fullH / 7        // radius y of circles along the sides
+			ofs2 := sixthH / 3      // offset away from the center, side circles
+			bh := fullH/4 + fullH/8 // placement of circles along the sides
+			// corners
+			svg.Ellipse(0-ofs1, 0-ofs1, br, br, color)
+			svg.Ellipse(0-ofs1, fullH+ofs1, br, br, color)
+			svg.Ellipse(fullW+ofs1, 0-ofs1, br, br, color)
+			svg.Ellipse(fullW+ofs1, fullH+ofs1, br, br, color)
+			// sides
+			svg.Ellipse(0-ofs2, bh, brx, bry, color)
+			svg.Ellipse(0-ofs2, fullH-bh, brx, bry, color)
+			svg.Ellipse(fullW+ofs2, bh, brx, bry, color)
+			svg.Ellipse(fullW+ofs2, fullH-bh, brx, bry, color)
 		case patternBricks: // Black/Dyed Field Masoned Banner
+			brickW := fullW / 4
+			brickH := fullH / 13
+			ofs := 0
+			for y := 0; y < fullH; y += brickH + 1 {
+				for x := ofs; x < fullW; x += brickW + 1 {
+					svg.Box(x, y, brickW, brickH, color)
+				}
+				// Alternate the x offset for every row of bricks
+				if ofs == 0 {
+					ofs = -brickH
+				} else {
+					ofs = 0
+				}
+			}
 		case patternGradientDown: // Gradient Banner
+			ur, _ := strconv.ParseUint(color[1:3], 16, 0)
+			ug, _ := strconv.ParseUint(color[3:5], 16, 0)
+			ub, _ := strconv.ParseUint(color[5:7], 16, 0)
+			r := int(ur)
+			g := int(ug)
+			b := int(ub)
+			a := 1.0
+			for y := 0; y < fullH; y++ {
+				a = 1.0 - (float64(y) / float64(fullH))
+				svg.Box(0, y, fullW, 1, onthefly.ColorStringAlpha(r, g, b, a))
+			}
 		case patternGradientUp: // Base Gradient Banner
+			ur, _ := strconv.ParseUint(color[1:3], 16, 0)
+			ug, _ := strconv.ParseUint(color[3:5], 16, 0)
+			ub, _ := strconv.ParseUint(color[5:7], 16, 0)
+			r := int(ur)
+			g := int(ug)
+			b := int(ub)
+			a := 1.0
+			for y := 0; y < fullH; y++ {
+				a = float64(y) / float64(fullH)
+				svg.Box(0, y, fullW, 1, onthefly.ColorStringAlpha(r, g, b, a))
+			}
 		case patternCreeper: // Black/Dyed Creeper Charge Banner
+			// eyes
+			svg.Box(sixthW-tenthW/2, thirdH-tenthH/2, halfW/2, halfW/2, color)
+			svg.Box(fullW-(sixthW+halfW/2)+tenthW/2, thirdH-tenthH/2, halfW/2, halfW/2, color)
+			// nose
+			svg.Line(halfW, thirdH+tenthH, halfW, halfH+(halfW/3), halfW/2, color)
+			// whiskers
+			svg.Box(sixthW+sixthW, thirdH+fifthH, tenthW, halfW/2, color)
+			svg.Box(fullW-(sixthW+sixthW)-tenthW, thirdH+fifthH, tenthW, halfW/2, color)
 		case patternSkull: // Black/Dyed Skull Charge Banner
 		case patternFlower: // Black/Dyed Flower Charge Banner
 		case patternLogo: // Black/Dyed Mojang Charge Banner
